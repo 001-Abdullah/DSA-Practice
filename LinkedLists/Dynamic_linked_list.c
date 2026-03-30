775456ba330692a83b03f51aa22f3e26cdc35971
Muhammad Abdullah <chabdullahayaz001@gmail.com>
2026-03-30 22:11:31 +0100

    This linked list displays numbers in same order as were entered

---
+++ LinkedLists/Dynamic_linked_list.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node
+{
+    int number;
+    struct node *next;
+} node;
+int main()
+{
+    node *head = NULL;
+    node *tail = NULL;
+    printf("(In the following code, enter -1 when done)\n");
+    while (1)
+    {
+        node *temp = malloc(sizeof(node));
+        if (temp == NULL)
+        {
+            return 1;
+        }
+        
+        printf("Enter the number: ");
+        scanf("%d", &temp->number);
+        temp->next = NULL;
+        if (temp->number == -1)
+        {
+            break;
+        }
+        else if (head == NULL)
+        {
+            head = temp;
+            tail = temp;
+        }
+        
+        else
+        {
+            tail->next = temp;
+            temp->next = NULL;
+            tail = temp;
+        }
+    }
+    for (node *ptr = head; ptr != NULL; ptr = ptr->next)
+    {
+        printf("%d ", ptr->number);
+    }
+    node *ptr = head;
+    while(ptr != NULL)
+    {
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
+    }
+    
+    return 0;
+}
